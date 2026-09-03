50038fe7508b127a3bac5aed6ff5c49f0359e363
Pablo Barreto do Nascimento <diariamentepablo@gmail.com>
2026-09-02 20:52:50 -0400

    Atividade 3
    - Adicionado o pulo duplo;
    - Adicionada a mecânica de agachar;
    - Mantém todas as capacidades de movimento de antes;
    - Transição de fases re-trabalhada.

--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -6,5 +6,4 @@ func _on_body_entered(_body: Node2D) -> void:
 	call_deferred("load_next_scene")
 
 func load_next_scene() -> void:
-	if is_inside_tree():
-		get_tree().change_scene_to_file("res://scenes/levels/" + next_level + ".tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/" + next_level + ".tscn")
